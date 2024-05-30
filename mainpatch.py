2ed84f7740e760e857406b8197e33664bd183446
SahnlongLinKing <arcticsnakeyt@gmail.com>
2024-05-29 20:11:10 -0400

    Add files via upload

--- mainpatch.py
+++ mainpatch.py
@@ -3,76 +3,66 @@ import asciihands
 
 i = 1
 while i < 2:
-    userchoice = input(
+    userchoice = int(input(
         "Let's play rock paper scissors. Press 1 for rock, 2 for paper, and 3 for scissors. "
-    )
+    ))
 
-    random_integer = random.randint(1, 3)
+    cpuchoice = random.randint(1, 3)
 
-    if random_integer == 1:
-        print(f"""I chose rock:
-    {asciihands.rock}
-    """)
-        if userchoice == "1":
-            print(f"""And you also chose rock:
-      {asciihands.rock}
-      """)
+    if userchoice == cpuchoice:
+        if userchoice == 1:
+            print(f"""We both chose rock:
+                  {asciihands.rock}""")
             print("It's a tie.")
-        elif userchoice == "2":
-            print(f"""And you chose paper:
-      {asciihands.paper}
-      """)
-            print("You win.")
-        elif userchoice == "3":
-            print(f"""And you chose scissors:
-      {asciihands.scissors}
-      """)
-            print("I win.")
-        else:
-            print("You didn't choose a valid option. You lose.")
-    elif random_integer == 2:
-        print(f"""I chose paper:
-    {asciihands.paper}
-    """)
-        if userchoice == "1":
-            print(f"""And you chose rock:
-      {asciihands.rock}
-      """)
-            print("I win.")
-        elif userchoice == "2":
-            print(f"""And you also chose paper:
-      {asciihands.paper}
-      """)
+        elif userchoice == 2:
+            print(f"""We both chose paper:
+                  {asciihands.paper}""")
             print("It's a tie.")
-        elif userchoice == "3":
-            print(f"""And you chose scissors:
-      {asciihands.scissors}
-      """)
-            print("You win.")
-        else:
-            print("You didn't choose a valid option. You lose.")
-    elif random_integer == 3:
-        print(f"""I chose paper:
-    {asciihands.paper}
-    """)
-        if userchoice == "1":
-            print(f"""And you chose rock:
-      {asciihands.rock}
-      """)
-            print("You win.")
-        elif userchoice == "2":
-            print(f"""And you also chose paper:
-      {asciihands.paper}
-      """)
-            print("I win.")
-        elif userchoice == "3":
-            print(f"""And you also chose scissors:
-      {asciihands.scissors}
-      """)
+        elif userchoice == 3:
+            print(f"""We both chose scissors:
+                  {asciihands.scissors}""")
             print("It's a tie.")
-        else:
-            print("You didn't choose a valid option. You lose.")
-
+    elif userchoice == 3 and cpuchoice == 1:
+        print(f"""You've chosen scissors:
+              {asciihands.scissors}""")
+        print(f"""And I chose rock:
+              {asciihands.rock}""")
+        print("I win.")
+    elif userchoice == 1 and cpuchoice == 3:
+        print(f"""You've chosen rock:
+              {asciihands.rock}""")
+        print(f"""And I chose scissors:
+              {asciihands.scissors}""")
+        print("I win.")
+    elif userchoice > cpuchoice:
+        if userchoice == 2:
+            print(f"""You chose paper:
+                  {asciihands.paper}""")
+            print(f"""And I chose rock:
+                  {asciihands.rock}""")
+            print("You win.")
+        elif userchoice == 2:
+            print(f"""You chose scissors:
+                  {asciihands.scissors}""")
+            print(f"""And I chose paper:
+                  {asciihands.paper}""")
+            print("You win.")
+    elif userchoice < cpuchoice:
+        if userchoice == 1:
+            print(f"""You chose rock:
+                  {asciihands.rock}""")
+            print(f"""And I chose paper:
+                  {asciihands.paper}""")
+            print("I win.")
+        elif userchoice == 2:
+            print(f"""You chose paper:
+                  {asciihands.paper}""")
+            print(f"""And I chose scissors:
+                  {asciihands.scissors}""")
+            print("I win.")
+    else:
+        print("""Either, you didn't choose a valid input, or my programming is failing.
+               If you chose a valid input, please contact arcticsnakeyt@gmail.com to report a bug.""")
     playagain = input("Play again? Y/N\n")
     if playagain.lower() == "y":
         i = 1
